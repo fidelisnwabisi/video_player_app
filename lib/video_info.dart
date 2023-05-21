d86ada38122c4d8463108e05fe81f5a541dbad5d
fidelis <fidelisnwabisi@gmail.com>
2023-05-21 10:56:59 +0100

    created video_info.dart file

---
+++ lib/video_info.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class VideoInfo extends StatefulWidget {
+  const VideoInfo({super.key});
+
+  @override
+  State<VideoInfo> createState() => _VideoInfoState();
+}
+
+class _VideoInfoState extends State<VideoInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(),
+      ),
+    );
+  }
+}
