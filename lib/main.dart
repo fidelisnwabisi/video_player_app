9b172f0b12f7eb01fdf8eaad707b68bbcd821c22
fidelis <fidelisnwabisi@gmail.com>
2023-05-21 11:19:18 +0100

    working on video_info page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player_app/video_info.dart';
 import 'home_page.dart';
 
 void main() {
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const VideoInfo(),
     );
   }
 }
--- lib/video_info.dart
+++ lib/video_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'colors.dart' as color;
 
 class VideoInfo extends StatefulWidget {
   const VideoInfo({super.key});
@@ -12,7 +13,38 @@ class _VideoInfoState extends State<VideoInfo> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.AppColor.gradientFirst.withOpacity(0.9),
+              color.AppColor.gradientSecond
+            ],
+            begin: const FractionalOffset(0.0, 0.4),
+            end: Alignment.topRight,
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 70, left: 30, right: 30),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.arrow_back_ios,
+                        size: 20,
+                        color: color.AppColor.secondPageIconColor,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
